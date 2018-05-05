2564b52a541dfb67a57e49effd85ebd2da846d0b
DingGe520R <784872490@qq.com>
2018-05-05 12:07:11 +0800

    5

--- specs/collectionOperator/get_letter_interval_spec.js
+++ specs/collectionOperator/get_letter_interval_spec.js
@@ -7,7 +7,7 @@ describe('get_letter_interval', function () {
   var collection_a = ['a', 'b', 'c', 'd', 'e'];
   var collection_b = ['e', 'd', 'c', 'b', 'a'];
 
-  it('根据给出的两个数字得到对应自增的字母区间', function() {
+  iit('根据给出的两个数字得到对应自增的字母区间', function() {
 
     var result = get_letter_interval(1, 5);
     expect(result).toEqual(collection_a);
