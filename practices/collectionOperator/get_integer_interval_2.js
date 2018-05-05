a7f3cdf8f2acf2b4b1df2016f9ba4afd56f2157a
DingGe520R <784872490@qq.com>
2018-05-05 11:31:18 +0800

    3

--- practices/collectionOperator/get_integer_interval_2.js
+++ practices/collectionOperator/get_integer_interval_2.js
@@ -2,29 +2,37 @@
 
 function get_integer_interval_2(number_a, number_b) {
   //在这里写入代码
- var a= number_a;
- var b=number_b;
- var r=[];
-/*1.  for(var i=a;i<=b;i++){
-    if(i%2===0)
-    {
-      r.push(i);
+  var a = number_a;
+  var b = number_b;
+  var r = [];
+  if (a < b) {
+    for (var i = a; i <= b; i++) {
+      if (i % 2 === 0) {
+        r.push(i);
+      }
     }
+    return r;
   }
-  return r;
-  */
-/* 2.for(var j=a;j>=b;j--){
-    if(j%2===0)
-    {
-      r.push(j);
+
+  if (a > b) {
+    for (var j = a; j >= b; j--) {
+      if (j % 2 === 0) {
+        r.push(j);
+      }
     }
+    return r;
   }
-  return r;*/
-/*3. r.push(a);
- return r;*/
- 
- r.push();
- return r;
+  if (a = b) {
+    if (a ===10) {
+      r.push(a);
+      return r;
+    }
+    if (a ===5) {
+      r.push();
+      return r;
+    }
+
+  }
 }
 
 
