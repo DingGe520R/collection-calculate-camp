738b3bf9974ecbf5cfc4ff1da757dff0979dc7c9
DingGe520R <784872490@qq.com>
2018-05-06 11:20:14 +0800

    5

--- practices/collectionOperator/get_union.js
+++ practices/collectionOperator/get_union.js
@@ -2,6 +2,17 @@
 
 function get_union(collection_a, collection_b) {
   //在这里写入代码
+  var r = [];
+  for (var i = 0; i < collection_a.length; i++) {
+    r.push(collection_a[i]);
+  }
+  for (var i = 0; i < collection_b.length; i++) {
+    if (!collection_a.includes(collection_b[i])) {
+      r.push(collection_b[i]);
+    }
+  }
+  return r;
+
 }
 
 module.exports = get_union;
