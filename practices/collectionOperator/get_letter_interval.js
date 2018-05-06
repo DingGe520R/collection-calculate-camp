d11124430a910cccda388bedd6b1de4f82aee315
DingGe520R <784872490@qq.com>
2018-05-06 10:19:03 +0800

    2

--- practices/collectionOperator/get_letter_interval.js
+++ practices/collectionOperator/get_letter_interval.js
@@ -2,13 +2,15 @@
 
 function get_letter_interval(number_a, number_b) {
   //在这里写入代码
-  var r=[];
-  var n=96;
-for(var i=0;i<number_b.length;i++){
-    n+=1;
-    r.push(n);
-}
-return String.fromCharCode(r);
+  var r = [];
+  var n = 97;
+  var k = 0;
+  for (var i = number_a; i <= number_b; i++) {
+    k = String.fromCharCode(n);
+    r.push(k);
+    n++;
+  }
+  return r;
 }
 
 module.exports = get_letter_interval;
