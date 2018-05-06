1608185d3f5606da6d6e86bdcc81de9fa3ce1a49
DingGe520R <784872490@qq.com>
2018-05-06 11:26:45 +0800

    5

--- practices/collectionOperator/get_union.js
+++ practices/collectionOperator/get_union.js
@@ -7,7 +7,7 @@ function get_union(collection_a, collection_b) {
     r.push(collection_a[i]);
   }
   for (var i = 0; i < collection_b.length; i++) {
-    if (!collection_a.includes(collection_b[i])) {
+    if (!collection_a.includes(collection_b[i])) {  //includes() 方法用来判断一个数组是否包含一个指定的值，如果是返回 true，否则false。
       r.push(collection_b[i]);
     }
   }
--- practices/filter/choose_even.js
+++ practices/filter/choose_even.js
@@ -3,6 +3,12 @@
 function choose_even(collection) {
 
   //在这里写入代码
+  var r=[];
+  for(var i=0;i<collection.length;i++){
+    if(collection[i]%2===0)
+     r.push(collection[i]);
+  }
+  return r;
 }
 
 module.exports = choose_even;
