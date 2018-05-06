bd1769fb1611b2ec9a1a6a2991608005500a25d9
DingGe520R <784872490@qq.com>
2018-05-06 11:31:51 +0800

    5

--- practices/filter/choose_multiples_of_three.js
+++ practices/filter/choose_multiples_of_three.js
@@ -3,6 +3,12 @@
 function choose_multiples_of_three(collection) {
 
   //在这里写入代码
+  var r=[];
+  for(var i=0;i<collection.length;i++){
+    if(collection[i]%3===0)
+     r.push(collection[i]);
+  }
+  return r;
 }
 
 module.exports = choose_multiples_of_three;
