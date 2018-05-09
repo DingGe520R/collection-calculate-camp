b319b561129a833f84f4d9b6a94bf2939f865631
DingGe520R <784872490@qq.com>
2018-05-09 21:51:29 +0800

    5

--- practices/filter/two_collections_practice_1.js
+++ practices/filter/two_collections_practice_1.js
@@ -1,8 +1,15 @@
 'use strict';
 
 function choose_common_elements(collection_a, collection_b) {
-
-  //在这里写入代码
+  var r = [];
+  for (var i = 0; i < collection_a.length; i++) {
+    for (var j = 0; j < collection_b.length; j++) {
+      if (collection_a[i] === collection_b[j]) {
+        r.push(collection_a[i]);
+      }
+    }
+  }
+  return r;
 }
 
 module.exports = choose_common_elements;
